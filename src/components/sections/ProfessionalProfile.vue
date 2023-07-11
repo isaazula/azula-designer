d880a720339c6548f8ab84ce6c18f0723c50d216
Isabella Azula <azula.designer@gmail.com>
2023-07-11 15:39:10 -0500

    ProfessionalProfile

---
+++ src/components/sections/ProfessionalProfile.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <section>
+    <p>
+      Soy productora gráfica digital de LaSalle College, he trabajado Freelance
+      en diferentes proyectos. Me caracterizo por proponer ideas creativas e
+      innovadoras, trabajar con esmero y en equipo, para fortalecer las ideas y
+      lograr la creación de excelentes propuestas. Me especializo en construir
+      la identidad gráfica de las marcas y comunicar la esencia de las mismas a
+      través del packaging, fotografía, animación 3D y producción de contenido
+      audiovisual.
+    </p>
+  </section>
+</template>
+
+<style scoped></style>
